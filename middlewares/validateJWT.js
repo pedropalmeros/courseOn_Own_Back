c98830d37a4caa9909109b33e60aae0029e1984c
Rods <rodolfo.perez01@iest.edu.mx>
2022-10-27 16:26:57 -0500

    elminar .deploy

--- middlewares/validateJWT.js
+++ middlewares/validateJWT.js
@@ -1,45 +1,43 @@
-const { request,response } = require('express');
+const { request, response } = require('express');
 const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
+const validateJWT = async (req = request, res = response, next) => {
+  console.log('req', req.header);
+  const token = req.header('userTok');
 
-const validateJWT = async(req = request, res = response, next) =>{
-    const token = req.header('userTok');
+  if (!token) {
+    return res.status(400).json({
+      msg: 'No token',
+    });
+  }
 
-    if( !token ){
-        return res.status(400).json({
-            msg: 'No token'
-        })
-   }
+  try {
+    const { id } = jwt.verify(token, process.env.SECRETORPIRVATEKEY);
 
-    try{
-        const {id} = jwt.verify(token,process.env.SECRETORPIRVATEKEY);
-  
-        const user = await User.findById(id);
+    const user = await User.findById(id);
 
-        if( !user){
-            return res.status(401).json({
-            msg: 'Invalid Token - user not in DB'
-        })
-        }
-
-
-        if(!user.status){
-            return res.status(401).json({
-            msg: 'Invalid Token - user status - false'
-            })
-        }
-        req.user = user;
-        next();
-   }
-    catch(error){
-        return res.status(400).json({
-            msg: "Error in the Token validation"
-        })
+    if (!user) {
+      return res.status(401).json({
+        msg: 'Invalid Token - user not in DB',
+      });
     }
-}
+
+    if (!user.status) {
+      return res.status(401).json({
+        msg: 'Invalid Token - user status - false',
+      });
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(400).json({
+      msg: 'Error in the Token validation',
+    });
+  }
+};
 
 module.exports = {
-    validateJWT
-}
+  validateJWT,
+};
